6c1ab30d61ecc985e1ef8a9dcd4203a1eaf2bc1e
tarsogithub <tarsocs@gmail.com>
2021-06-15 13:45:02 -0300

    Fim do desafio

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const section1 = document.getElementById("section1");
-const section2 = document.getElementById("section2");
-const section3 = document.getElementById("section3");
-const section4 = document.getElementById("section4");
+const section1 = document.querySelector("#section1");
+const section2 = document.querySelector("#section2");
+const section3 = document.querySelector("#section3");
+const section4 = document.querySelector("#section4");
 
 geraValorAleatorio = () => {
   return Math.floor(Math.random() * 671);
